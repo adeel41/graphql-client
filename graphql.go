290284a48c0ec148d008d60d89988c4ec45c946b
Adeel Mahmood <adeel41@yahoo.com>
2021-08-28 11:53:56 +0100

    Added basic logging

--- graphql.go
+++ graphql.go
@@ -6,10 +6,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 
 	"golang.org/x/net/context/ctxhttp"
 )
+
+var logger *log.Logger
+
+func init() {
+	logger = log.New(os.Stderr, "graphql-client", log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 // Client is a client for interacting with a GraphQL API.
 type Client struct {
@@ -45,24 +53,37 @@ func (c *Client) RawRequest(ctx context.Context, query string, variables map[str
 		return nil, err
 	}
 
+	logger.Printf("[POST] %v\r\n%q\r\n", c.url, buf.Bytes())
 	responseContent, err := ctxhttp.Post(ctx, c.httpClient, c.url, "application/json", &buf)
+	var errBody []byte
 	if err != nil {
+		errBody, _ = ioutil.ReadAll(responseContent.Body)
+		logger.Printf("%v status code. %q\r\n%s\r\n", responseContent.Status, err.Error(), errBody)
 		return &GraphQLResponse{
 			StatusCode: responseContent.StatusCode,
 		}, err
 	}
 	defer responseContent.Body.Close()
 
-	body, _ := ioutil.ReadAll(responseContent.Body)
+	var body []byte
+	if errBody != nil {
+		body = errBody
+	} else {
+		body, _ = ioutil.ReadAll(responseContent.Body)
+	}
+
 	if responseContent.StatusCode != http.StatusOK {
+		errString := fmt.Sprintf("%v status code\r\n%s", responseContent.Status, body)
+		logger.Println(errString)
 		return &GraphQLResponse{
 			StatusCode:      responseContent.StatusCode,
 			ResponseContent: body,
-		}, fmt.Errorf("non-200 OK status code: %v body: %q", responseContent.Status, body)
+		}, fmt.Errorf(errString)
 	}
 
 	result, err := decodeGraphQLResponse(body)
 	if err != nil {
+		logger.Printf("response decoding error: %s\r\n", err.Error())
 		return nil, err
 	}
 
